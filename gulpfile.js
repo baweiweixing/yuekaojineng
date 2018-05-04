d80fd99041bf3097414b25fa674556f4f1069a36
weixing <2659177786@qq.com>
2018-05-04 08:36:47 +0800

    准备C

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var server = require("gulp-webserver");
+// var datajson = require("src/data/data.json");
 
 gulp.task("default", function() {
     gulp.src("src")
@@ -8,6 +9,9 @@ gulp.task("default", function() {
             open: true,
             livereload: true,
             middleware: function(req, res, next) {
+                // if (req.url === "/index") {
+                //     res.end(JSON.stringify(datajson))
+                // }
                 next();
             }
         }))
