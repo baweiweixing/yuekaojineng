dd3aeeee8436754d6af4087e93278038f575179d
weixing <2659177786@qq.com>
2018-05-04 09:30:00 +0800

    zuihou

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,1 +1,8 @@
-console.log("fff")
+var W = $(".list li").outerWidth(true)
+var len = $(".list li").length
+$(".list").css("width", W * len + "px")
+
+var myscroll = new IScroll(".scroll-box", {
+    scrollX: true,
+    scrollY: false
+})
